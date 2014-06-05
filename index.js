caae34da88aa14f6e4905ad6247fe767c3dc7c8f
KyeongJin Kim <kyeongjin.kim@treepla.net>
2014-06-05 12:03:23 +0900

    애저에 맞게 소스 수정.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 ﻿var http = require('http');
+var port = process.env.PORT;
 http.createServer(function(request, response) {
-	response.writeHead(200);
-	response.write("Hello, this is dog.");
-	response.end();
-}).listen(3000);
-console.log('Listening on port 3000...');
+	response.writeHead(200, { 'Content-Type' : 'text/plain' });
+	response.end('Hello World\n');
+}).listen(port);
