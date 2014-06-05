c764cbfc48a631b00c2f293346f651275c275c3c
KyeongJin Kim <kyeongjin.kim@treepla.net>
2014-06-05 11:57:12 +0900

    index.js 테스트 node.js 생성.

---
+++ index.js
@@ -0,0 +1,7 @@
+﻿var http = require('http');
+http.createServer(function(request, response) {
+	response.writeHead(200);
+	response.write("Hello, this is dog.");
+	response.end();
+}).listen(3000);
+console.log('Listening on port 3000...');
