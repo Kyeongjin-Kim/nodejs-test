d976475b44758699e8590e2ccf530c535f6157cc
KyeongJin Kim <kyeongjin.kim@treepla.net>
2014-06-05 12:08:29 +0900

    재커밋.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-﻿var http = require('http');
-var port = process.env.PORT;
+﻿var http = require('http')
+var port = process.env.PORT
 http.createServer(function(request, response) {
 	response.writeHead(200, { 'Content-Type' : 'text/plain' });
 	response.end('Hello World\n');
