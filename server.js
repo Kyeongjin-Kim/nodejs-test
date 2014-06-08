69b58b88d59275e95d7521cc7a1614ac995edbcb
KyeongJin Kim <kyeongjin.kim@treepla.net>
2014-06-08 17:20:49 +0900

    node.js test2

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-﻿﻿var http = require('http')
+﻿console.log('test');
+
+/*
+﻿var http = require('http')
 var port = process.env.PORT
 http.createServer(function(request, response) {
 	response.writeHead(200, { 'Content-Type' : 'text/plain' });
 	response.end('Hello World node.js\n');
 }).listen(port);
+*/
