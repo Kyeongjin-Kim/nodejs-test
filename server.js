db9ec241c06ae8214450caa2644d6d77c4bb9e35
KyeongJin Kim <kyeongjin.kim@treepla.net>
2014-06-08 17:04:12 +0900

    test

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-﻿console.log('Server running at http://127.0.0.1:1337/');
+﻿﻿var http = require('http')
+var port = process.env.PORT
+http.createServer(function(request, response) {
+	response.writeHead(200, { 'Content-Type' : 'text/plain' });
+	response.end('Hello World\n');
+}).listen(port);
