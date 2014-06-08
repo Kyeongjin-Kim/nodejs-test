9f95021c1ccfa8df2ae492fc81d6015de708d1f2
KyeongJin Kim <kyeongjin.kim@treepla.net>
2014-06-08 18:10:27 +0900

    netbeans 개발툴 테스트1

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
-﻿console.log('test');
-
-/*
-﻿var http = require('http')
-var port = process.env.PORT
+var http = require('http');
+var port = process.env.PORT;
+//var port = 50002;
 http.createServer(function(request, response) {
 	response.writeHead(200, { 'Content-Type' : 'text/plain' });
-	response.end('Hello World node.js\n');
+	response.end('Hello World !!!!\n');
 }).listen(port);
-*/
