a035762bb39349b5bfd5d70202a60ad73f405206
KyeongJin Kim <kyeongjin.kim@treepla.net>
2014-06-08 15:41:54 +0900

    기존 샘플 삭제.

--- server.js
+++
@@ -1,6 +0,0 @@
-﻿var http = require('http')
-var port = process.env.PORT
-http.createServer(function(request, response) {
-	response.writeHead(200, { 'Content-Type' : 'text/plain' });
-	response.end('Hello World\n');
-}).listen(port);
--- test.php
+++
@@ -1,8 +0,0 @@
-﻿<html>
- <head>
-  <title>PHP 테스트</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?> 
- </body>
-</html>
